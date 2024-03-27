a7df202eaab90637d420cedfd9ea75db48154580
Abdullah Bedwey <bedweyy@gmail.com>
2024-03-27 20:34:52 +0200

    Add shebang and module docstring to web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+'''A module for using the Redis NoSQL data storage.
+'''
+
 import requests
 import redis
 
@@ -5,16 +9,45 @@ r = redis.Redis()
 
 
 def count_calls(url: str):
+    """
+    Decorator that counts the number of times a 3
+    function is called with a specific URL.
+
+    Args:
+        func (Callable): The function to be decorated.
+
+    Returns:
+        Callable: The decorated function.
+    """
     key = f"count:{url}"
     r.incr(key)
 
 
 def cache_page(url: str, content: str):
+    """
+    Decorator that caches the result of a 
+    function call with a specific URL.
+
+    Args:
+        func (Callable): The function to be decorated.
+
+    Returns:
+        Callable: The decorated function.
+    """
     key = f"page:{url}"
     r.setex(key, 10, content)
 
 
 def get_page(url: str) -> str:
+    """
+    Fetches the HTML content of a URL.
+
+    Args:
+        url (str): The URL to fetch.
+
+    Returns:
+        str: The HTML content of the URL.
+    """
     count_calls(url)
     page = r.get(f"page:{url}")
     if page is None:
